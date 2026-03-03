7788cc0e281cf11f70c321920e6efc216f2f3b22
jordan inskeep <jinskeep@morpc.org>
2026-03-03 13:41:48 -0500

    fix relationships in models.

--- doc/app.py
+++ doc/app.py
@@ -20,139 +20,52 @@ def on_startup():
 def get_session():
     with Session(engine) as session:
         yield session
+
+# @app.get('/')
+# def read_schema(*, session: Session = Depends(get_session)):
+#     return {name.lower()}s
+
     
-# Sensor requests
-@app.post('/sensors/', response_model=SensorPublic)
-def create_sensor(*, session: Session = Depends(get_session), sensor: SensorCreate):
-    sensor = Sensor.model_validate(sensor)
-    session.add(sensor)
+# Location requests
+@app.post('/locations/', response_model=LocationPublic)
+def create_location(*, session: Session = Depends(get_session), location: LocationCreate):
+    location = Location.model_validate(location)
+    session.add(location)
     session.commit()
-    session.refresh(sensor)
-    return sensor
+    session.refresh(location)
+    return location
     
-@app.get('/sensors/', response_model=list[SensorPublic])
-def read_sensors(*, session: Session = Depends(get_session)):
-    sensors = session.exec(select(Sensor)).all()
-    return sensors
+@app.get('/locations/', response_model=list[LocationPublic])
+def read_locations(*, session: Session = Depends(get_session)):
+    locations = session.exec(select(Location)).all()
+    return locations
     
-@app.get('/sensors/{sensor_macaddr}', response_model=SensorPublic)
-def read_sensor(*, session: Session = Depends(get_session), sensor_macaddr: str):
-    sensor = session.get(Sensor, sensor_macaddr)
-    if not sensor:
-        raise HTTPException(status_code=404, detail='Sensor not found.')
-    return sensor
+@app.get('/locations/{location_address}', response_model=LocationPublic)
+def read_location(*, session: Session = Depends(get_session), location_address: str):
+    location = session.get(Location, location_address)
+    if not location:
+        raise HTTPException(status_code=404, detail='Location not found.')
+    return location
     
     
-@app.patch('/sensors/{sensor_macaddr}', response_model=SensorPublic)
-def update_sensor(*, session: Session = Depends(get_session), sensor_macaddr: str, sensor: SensorUpdate):
-    db_sensor = session.get(Sensor, sensor_macaddr)
-    if not db_sensor:
-        raise HTTPException(status_code=404, detail=f'Sensor {sensor_macaddr} not found.')
-    sensor_data = sensor.model_dump(exclude_unset=True)
-    db_sensor.sqlmodel_update(sensor_data)
-    session.add(db_sensor)
+@app.patch('/locations/{location_address}', response_model=LocationPublic)
+def update_location(*, session: Session = Depends(get_session), location_address: str, location: LocationUpdate):
+    db_location = session.get(Location, location_address)
+    if not db_location:
+        raise HTTPException(status_code=404, detail=f'Location {location_address} not found.')
+    location_data = location.model_dump(exclude_unset=True)
+    db_location.sqlmodel_update(location_data)
+    session.add(db_location)
     session.commit()
-    session.refresh(db_sensor)
-    return db_sensor
+    session.refresh(db_location)
+    return db_location
     
-@app.delete('/sensors/{sensor_macaddr}')
-def delete_sensor(*, session: Session = Depends(get_session), sensor_macaddr: str):
-    sensor = session.get(Sensor, sensor_macaddr)
-    if not sensor:
-        raise HTTPException(status_code=404, detail=f'Sensor {sensor_macaddr} not found.')
-    session.delete(sensor)
-    session.commit()
-    return {'ok': True}
-    
-    
-# Deployment requests
-@app.post('/deployments/', response_model=DeploymentPublic)
-def create_deployment(*, session: Session = Depends(get_session), deployment: DeploymentCreate):
-    deployment = Deployment.model_validate(deployment)
-    session.add(deployment)
-    session.commit()
-    session.refresh(deployment)
-    return deployment
-    
-@app.get('/deployments/', response_model=list[DeploymentPublicWithAll])
-def read_deployments(*, session: Session = Depends(get_session)):
-    deployments = session.exec(select(Deployment)).all()
-    return deployments
-    
-@app.get('/deployments/{deployment_name}', response_model=DeploymentPublicWithAll)
-def read_deployment(*, session: Session = Depends(get_session), deployment_name: str):
-    deployment = session.get(Deployment, deployment_name)
-    if not deployment:
-        raise HTTPException(status_code=404, detail='Deployment not found.')
-    return deployment
-    
-@app.get('/deployments/query', response_model=list[DeploymentPublicWithAll])
-async def query_deployments(*, session: AsyncSession = Depends(get_session), query=QueryBuilder(Deployment)):
-    deployments = session.execute(query)
-    return deployments.scalars().all()
-    
-@app.patch('/deployments/{deployment_name}', response_model=DeploymentPublic)
-def update_deployment(*, session: Session = Depends(get_session), deployment_name: str, deployment: DeploymentUpdate):
-    db_deployment = session.get(Deployment, deployment_name)
-    if not db_deployment:
-        raise HTTPException(status_code=404, detail=f'Deployment {deployment_name} not found.')
-    deployment_data = deployment.model_dump(exclude_unset=True)
-    db_deployment.sqlmodel_update(deployment_data)
-    session.add(db_deployment)
-    session.commit()
-    session.refresh(db_deployment)
-    return db_deployment
-    
-@app.delete('/deployments/{deployment_name}')
-def delete_deployment(*, session: Session = Depends(get_session), deployment_name: str):
-    deployment = session.get(Deployment, deployment_name)
-    if not deployment:
-        raise HTTPException(status_code=404, detail=f'Deployment {deployment_name} not found.')
-    session.delete(deployment)
-    session.commit()
-    return {'ok': True}
-    
-    
-# Contact requests
-@app.post('/contacts/', response_model=ContactPublic)
-def create_contact(*, session: Session = Depends(get_session), contact: ContactCreate):
-    contact = Contact.model_validate(contact)
-    session.add(contact)
-    session.commit()
-    session.refresh(contact)
-    return contact
-    
-@app.get('/contacts/', response_model=list[ContactPublic])
-def read_contacts(*, session: Session = Depends(get_session)):
-    contacts = session.exec(select(Contact)).all()
-    return contacts
-    
-@app.get('/contacts/{contact_fullname}', response_model=ContactPublic)
-def read_contact(*, session: Session = Depends(get_session), contact_fullname: str):
-    contact = session.get(Contact, contact_fullname)
-    if not contact:
-        raise HTTPException(status_code=404, detail='Contact not found.')
-    return contact
-    
-    
-@app.patch('/contacts/{contact_fullname}', response_model=ContactPublic)
-def update_contact(*, session: Session = Depends(get_session), contact_fullname: str, contact: ContactUpdate):
-    db_contact = session.get(Contact, contact_fullname)
-    if not db_contact:
-        raise HTTPException(status_code=404, detail=f'Contact {contact_fullname} not found.')
-    contact_data = contact.model_dump(exclude_unset=True)
-    db_contact.sqlmodel_update(contact_data)
-    session.add(db_contact)
-    session.commit()
-    session.refresh(db_contact)
-    return db_contact
-    
-@app.delete('/contacts/{contact_fullname}')
-def delete_contact(*, session: Session = Depends(get_session), contact_fullname: str):
-    contact = session.get(Contact, contact_fullname)
-    if not contact:
-        raise HTTPException(status_code=404, detail=f'Contact {contact_fullname} not found.')
-    session.delete(contact)
+@app.delete('/locations/{location_address}')
+def delete_location(*, session: Session = Depends(get_session), location_address: str):
+    location = session.get(Location, location_address)
+    if not location:
+        raise HTTPException(status_code=404, detail=f'Location {location_address} not found.')
+    session.delete(location)
     session.commit()
     return {'ok': True}
     
@@ -197,6 +110,50 @@ def delete_hotspot(*, session: Session = Depends(get_session), hotspot_macaddr: 
     if not hotspot:
         raise HTTPException(status_code=404, detail=f'Hotspot {hotspot_macaddr} not found.')
     session.delete(hotspot)
+    session.commit()
+    return {'ok': True}
+    
+    
+# Sensor requests
+@app.post('/sensors/', response_model=SensorPublic)
+def create_sensor(*, session: Session = Depends(get_session), sensor: SensorCreate):
+    sensor = Sensor.model_validate(sensor)
+    session.add(sensor)
+    session.commit()
+    session.refresh(sensor)
+    return sensor
+    
+@app.get('/sensors/', response_model=list[SensorPublic])
+def read_sensors(*, session: Session = Depends(get_session)):
+    sensors = session.exec(select(Sensor)).all()
+    return sensors
+    
+@app.get('/sensors/{sensor_macaddr}', response_model=SensorPublic)
+def read_sensor(*, session: Session = Depends(get_session), sensor_macaddr: str):
+    sensor = session.get(Sensor, sensor_macaddr)
+    if not sensor:
+        raise HTTPException(status_code=404, detail='Sensor not found.')
+    return sensor
+    
+    
+@app.patch('/sensors/{sensor_macaddr}', response_model=SensorPublic)
+def update_sensor(*, session: Session = Depends(get_session), sensor_macaddr: str, sensor: SensorUpdate):
+    db_sensor = session.get(Sensor, sensor_macaddr)
+    if not db_sensor:
+        raise HTTPException(status_code=404, detail=f'Sensor {sensor_macaddr} not found.')
+    sensor_data = sensor.model_dump(exclude_unset=True)
+    db_sensor.sqlmodel_update(sensor_data)
+    session.add(db_sensor)
+    session.commit()
+    session.refresh(db_sensor)
+    return db_sensor
+    
+@app.delete('/sensors/{sensor_macaddr}')
+def delete_sensor(*, session: Session = Depends(get_session), sensor_macaddr: str):
+    sensor = session.get(Sensor, sensor_macaddr)
+    if not sensor:
+        raise HTTPException(status_code=404, detail=f'Sensor {sensor_macaddr} not found.')
+    session.delete(sensor)
     session.commit()
     return {'ok': True}
     
@@ -249,46 +206,94 @@ def delete_sensornote(*, session: Session = Depends(get_session), sensornote_id:
     return {'ok': True}
     
     
-# Location requests
-@app.post('/locations/', response_model=LocationPublic)
-def create_location(*, session: Session = Depends(get_session), location: LocationCreate):
-    location = Location.model_validate(location)
-    session.add(location)
+# Contact requests
+@app.post('/contacts/', response_model=ContactPublic)
+def create_contact(*, session: Session = Depends(get_session), contact: ContactCreate):
+    contact = Contact.model_validate(contact)
+    session.add(contact)
     session.commit()
-    session.refresh(location)
-    return location
+    session.refresh(contact)
+    return contact
     
-@app.get('/locations/', response_model=list[LocationPublic])
-def read_locations(*, session: Session = Depends(get_session)):
-    locations = session.exec(select(Location)).all()
-    return locations
+@app.get('/contacts/', response_model=list[ContactPublic])
+def read_contacts(*, session: Session = Depends(get_session)):
+    contacts = session.exec(select(Contact)).all()
+    return contacts
     
-@app.get('/locations/{location_address}', response_model=LocationPublic)
-def read_location(*, session: Session = Depends(get_session), location_address: str):
-    location = session.get(Location, location_address)
-    if not location:
-        raise HTTPException(status_code=404, detail='Location not found.')
-    return location
+@app.get('/contacts/{contact_fullname}', response_model=ContactPublic)
+def read_contact(*, session: Session = Depends(get_session), contact_fullname: str):
+    contact = session.get(Contact, contact_fullname)
+    if not contact:
+        raise HTTPException(status_code=404, detail='Contact not found.')
+    return contact
     
     
-@app.patch('/locations/{location_address}', response_model=LocationPublic)
-def update_location(*, session: Session = Depends(get_session), location_address: str, location: LocationUpdate):
-    db_location = session.get(Location, location_address)
-    if not db_location:
-        raise HTTPException(status_code=404, detail=f'Location {location_address} not found.')
-    location_data = location.model_dump(exclude_unset=True)
-    db_location.sqlmodel_update(location_data)
-    session.add(db_location)
+@app.patch('/contacts/{contact_fullname}', response_model=ContactPublic)
+def update_contact(*, session: Session = Depends(get_session), contact_fullname: str, contact: ContactUpdate):
+    db_contact = session.get(Contact, contact_fullname)
+    if not db_contact:
+        raise HTTPException(status_code=404, detail=f'Contact {contact_fullname} not found.')
+    contact_data = contact.model_dump(exclude_unset=True)
+    db_contact.sqlmodel_update(contact_data)
+    session.add(db_contact)
     session.commit()
-    session.refresh(db_location)
-    return db_location
+    session.refresh(db_contact)
+    return db_contact
     
-@app.delete('/locations/{location_address}')
-def delete_location(*, session: Session = Depends(get_session), location_address: str):
-    location = session.get(Location, location_address)
-    if not location:
-        raise HTTPException(status_code=404, detail=f'Location {location_address} not found.')
-    session.delete(location)
+@app.delete('/contacts/{contact_fullname}')
+def delete_contact(*, session: Session = Depends(get_session), contact_fullname: str):
+    contact = session.get(Contact, contact_fullname)
+    if not contact:
+        raise HTTPException(status_code=404, detail=f'Contact {contact_fullname} not found.')
+    session.delete(contact)
+    session.commit()
+    return {'ok': True}
+    
+    
+# Deployment requests
+@app.post('/deployments/', response_model=DeploymentPublic)
+def create_deployment(*, session: Session = Depends(get_session), deployment: DeploymentCreate):
+    deployment = Deployment.model_validate(deployment)
+    session.add(deployment)
+    session.commit()
+    session.refresh(deployment)
+    return deployment
+    
+@app.get('/deployments/', response_model=list[DeploymentPublicWithAll])
+def read_deployments(*, session: Session = Depends(get_session)):
+    deployments = session.exec(select(Deployment)).all()
+    return deployments
+    
+@app.get('/deployments/{deployment_name}', response_model=DeploymentPublicWithAll)
+def read_deployment(*, session: Session = Depends(get_session), deployment_name: str):
+    deployment = session.get(Deployment, deployment_name)
+    if not deployment:
+        raise HTTPException(status_code=404, detail='Deployment not found.')
+    return deployment
+    
+@app.get('/deployments/query', response_model=list[DeploymentPublicWithAll])
+async def query_deployments(*, session: AsyncSession = Depends(get_session), query=QueryBuilder(Deployment)):
+    deployments = session.execute(query)
+    return deployments.scalars().all()
+    
+@app.patch('/deployments/{deployment_name}', response_model=DeploymentPublic)
+def update_deployment(*, session: Session = Depends(get_session), deployment_name: str, deployment: DeploymentUpdate):
+    db_deployment = session.get(Deployment, deployment_name)
+    if not db_deployment:
+        raise HTTPException(status_code=404, detail=f'Deployment {deployment_name} not found.')
+    deployment_data = deployment.model_dump(exclude_unset=True)
+    db_deployment.sqlmodel_update(deployment_data)
+    session.add(db_deployment)
+    session.commit()
+    session.refresh(db_deployment)
+    return db_deployment
+    
+@app.delete('/deployments/{deployment_name}')
+def delete_deployment(*, session: Session = Depends(get_session), deployment_name: str):
+    deployment = session.get(Deployment, deployment_name)
+    if not deployment:
+        raise HTTPException(status_code=404, detail=f'Deployment {deployment_name} not found.')
+    session.delete(deployment)
     session.commit()
     return {'ok': True}
     
--- doc/models.py
+++ doc/models.py
@@ -26,6 +26,60 @@ class TimestampMixin: # https://www.davidmuraya.com/blog/reusable-sqlmodel-mixin
         sa_type=DateTime(timezone=True)
     )
 
+## Location models
+class LocationBase(SQLModel):
+    address: str = Field(primary_key = True)
+    zipcode: int
+    neighborhood: str
+    description: str
+    latitude: float
+    longitude: float
+
+class Location(LocationBase, TimestampMixin, table=True):
+    deployments: list['Deployment'] | None = Relationship(back_populates='locations')
+
+class LocationCreate(LocationBase):
+    pass
+
+class LocationPublic(LocationBase):
+    created_at: datetime
+    updated_at: datetime
+
+
+class LocationUpdate(LocationBase):
+    zipcode: int | None
+    neighborhood: str | None
+    description: str | None
+    latitude: float | None
+    longitude: float | None
+
+    
+## Hotspot models
+class HotspotBase(SQLModel):
+    macaddr: str = Field(primary_key = True)
+    serialnum: str
+    status: str
+    ssid: float
+    password: float
+
+class Hotspot(HotspotBase, TimestampMixin, table=True):
+    deployments: list['Deployment'] | None = Relationship(back_populates='hotspots')
+
+class HotspotCreate(HotspotBase):
+    pass
+
+class HotspotPublic(HotspotBase):
+    created_at: datetime
+    updated_at: datetime
+
+
+class HotspotUpdate(HotspotBase):
+    serialnum: str | None
+    status: str | None
+    ssid: float | None
+    password: float | None
+
+    
 ## Sensor models
 class SensorBase(SQLModel):
     macaddr: str = Field(primary_key = True)
@@ -34,8 +88,8 @@ class SensorBase(SQLModel):
     status: str | None
 
 class Sensor(SensorBase, TimestampMixin, table=True):
-    deployments: list['Deployment'] | None = Relationship(back_populates='sensor')
-    sensornotes: list['SensorNote'] | None = Relationship(back_populates='sensor')
+    sensornotes: list['SensorNote'] | None = Relationship(back_populates='sensors')
+    deployments: list['Deployment'] | None = Relationship(back_populates='sensors')
 
 class SensorCreate(SensorBase):
     pass
@@ -51,19 +105,73 @@ class SensorUpdate(SensorBase):
     status: str | None
 
     
+## SensorNote models
+class SensorNoteBase(SQLModel):
+    sensor_name: str = Field(foreign_key='sensor.name')
+    date: date
+    author: str
+    note: str
+
+class SensorNote(SensorNoteBase, TimestampMixin, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    sensors: list['Sensor'] | None = Relationship(back_populates='sensornotes')
+
+class SensorNoteCreate(SensorNoteBase):
+    pass
+
+class SensorNotePublic(SensorNoteBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
+
+class SensorNotePublicWithAll(SensorNotePublic):
+    sensors: list['Sensor'] | None
+
+class SensorNoteUpdate(SensorNoteBase):
+    sensor_name: str | None
+    date: date | None
+    author: str | None
+    note: str | None
+
+    
+## Contact models
+class ContactBase(SQLModel):
+    fullname: str = Field(primary_key = True)
+    email: str | None
+    phone: str | None
+
+class Contact(ContactBase, TimestampMixin, table=True):
+    deployments: list['Deployment'] | None = Relationship(back_populates='contacts')
+
+class ContactCreate(ContactBase):
+    pass
+
+class ContactPublic(ContactBase):
+    created_at: datetime
+    updated_at: datetime
+
+
+class ContactUpdate(ContactBase):
+    email: str | None
+    phone: str | None
+
+    
 ## Deployment models
 class DeploymentBase(SQLModel):
     name: str = Field(primary_key = True)
-    sensor_name: str
+    sensor_name: str = Field(foreign_key='sensor.name')
     sensor_index: int | None
     start_date: date | None
     end_date: date | None
-    location_address: str | None
-    hotspot_macaddr: str | None
-    contact_fullname: str | None
+    location_address: str | None = Field(foreign_key='location.address')
+    hotspot_macaddr: str | None = Field(foreign_key='hotspot.macaddr')
+    contact_fullname: str | None = Field(foreign_key='contact.fullname')
 
 class Deployment(DeploymentBase, TimestampMixin, table=True):
-    pass
+    locations: list['Location'] | None = Relationship(back_populates='deployments')
+    contacts: list['Contact'] | None = Relationship(back_populates='deployments')
+    hotspots: list['Hotspot'] | None = Relationship(back_populates='deployments')
+    sensors: list['Sensor'] | None = Relationship(back_populates='deployments')
 
 class DeploymentCreate(DeploymentBase):
     pass
@@ -86,109 +194,5 @@ class DeploymentUpdate(DeploymentBase):
     location_address: str | None
     hotspot_macaddr: str | None
     contact_fullname: str | None
-
-    
-## Contact models
-class ContactBase(SQLModel):
-    fullname: str = Field(primary_key = True)
-    email: str | None
-    phone: str | None
-
-class Contact(ContactBase, TimestampMixin, table=True):
-    deployments: list['Deployment'] | None = Relationship(back_populates='contact')
-
-class ContactCreate(ContactBase):
-    pass
-
-class ContactPublic(ContactBase):
-    created_at: datetime
-    updated_at: datetime
-
-
-class ContactUpdate(ContactBase):
-    email: str | None
-    phone: str | None
-
-    
-## Hotspot models
-class HotspotBase(SQLModel):
-    macaddr: str = Field(primary_key = True)
-    serialnum: str
-    status: str
-    ssid: float
-    password: float
-
-class Hotspot(HotspotBase, TimestampMixin, table=True):
-    deployments: list['Deployment'] | None = Relationship(back_populates='hotspot')
-
-class HotspotCreate(HotspotBase):
-    pass
-
-class HotspotPublic(HotspotBase):
-    created_at: datetime
-    updated_at: datetime
-
-
-class HotspotUpdate(HotspotBase):
-    serialnum: str | None
-    status: str | None
-    ssid: float | None
-    password: float | None
-
-    
-## SensorNote models
-class SensorNoteBase(SQLModel):
-    sensor_name: str
-    date: date
-    author: str
-    note: str
-
-class SensorNote(SensorNoteBase, TimestampMixin, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-
-class SensorNoteCreate(SensorNoteBase):
-    pass
-
-class SensorNotePublic(SensorNoteBase):
-    id: int
-    created_at: datetime
-    updated_at: datetime
-
-class SensorNotePublicWithAll(SensorNotePublic):
-    sensors: list['Sensor'] | None
-
-class SensorNoteUpdate(SensorNoteBase):
-    sensor_name: str | None
-    date: date | None
-    author: str | None
-    note: str | None
-
-    
-## Location models
-class LocationBase(SQLModel):
-    address: str = Field(primary_key = True)
-    zipcode: int
-    neighborhood: str
-    description: str
-    latitude: float
-    longitude: float
-
-class Location(LocationBase, TimestampMixin, table=True):
-    deployments: list['Deployment'] | None = Relationship(back_populates='location')
-
-class LocationCreate(LocationBase):
-    pass
-
-class LocationPublic(LocationBase):
-    created_at: datetime
-    updated_at: datetime
-
-
-class LocationUpdate(LocationBase):
-    zipcode: int | None
-    neighborhood: str | None
-    description: str | None
-    latitude: float | None
-    longitude: float | None
 
     
